73e0b56e4761f3cb06492cbb0eb8f1906688ff58
jvcoutinho <joao.coutinho9@gmail.com>
2019-12-20 12:08:30 -0300

    Updates

---
+++ precache-manifest.29594a87718c540d156fef8f9a06eb80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd035fd66df3fa08f48cbc06eb1dc6f8",
+    "url": "/personal-page/index.html"
+  },
+  {
+    "revision": "53c02dd411a87fde05d0",
+    "url": "/personal-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad4c268b04de6f0c432b",
+    "url": "/personal-page/static/js/2.512ed4ce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/personal-page/static/js/2.512ed4ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53c02dd411a87fde05d0",
+    "url": "/personal-page/static/js/main.94e8eb9e.chunk.js"
+  },
+  {
+    "revision": "69d4cc3a9ffce613670f",
+    "url": "/personal-page/static/js/runtime-main.4051e6e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
