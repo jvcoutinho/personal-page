b7af7ff4e85c7a7834e0d3b44fb9d7f8eec48ea8
jvcoutinho <joao.coutinho9@gmail.com>
2019-12-22 23:11:47 -0300

    Setting up typewriter and adjusting background

--- src/components/MidContent.js
+++ src/components/MidContent.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import './MidContent.scss'
 import Container from 'react-bootstrap/Container'
+import Typewriter from 'typewriter-effect'
 
 const MidContent = () => {
+    let gameProgrammerText = buildMultiColorText('Game', ["#05e9e8", "#ff890b", "#dc80b1", "#e91313"]) + ' Programmer'
+
     return (
         <Container id="mid-content">
-            <h1 className="display-4">Hi there, I'm João Victor</h1>
-            <h2>didi</h2>
+            <h1 className="display-4">Hi there, I'm João</h1>
+            <Typewriter
+                options={{
+                    strings: [gameProgrammerText, 'Software Engineer'],
+                    autoStart: true,
+                    loop: true
+                }} />
         </Container>
     )
+}
+
+const buildMultiColorText = (text, colors) => {
+    let result = ''
+    for (let i = 0; i < text.length; i++) {
+        result += "<span style=\'color: " + colors[i % colors.length] + ";\'>" + text[i] + "</span>"
+    }
+    return result
 }
 
 export default MidContent
