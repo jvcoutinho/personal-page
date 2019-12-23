dc50c22478a412278bc9f5ada88584a638914788
jvcoutinho <joao.coutinho9@gmail.com>
2019-12-23 07:17:15 -0300

    Putting LinkedIn button on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,8 @@ import BNavbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import './Navbar.scss'
+import { FaLinkedin } from 'react-icons/fa'
+import { IconContext } from "react-icons";
 
 const Navbar = () => {
     return (
@@ -17,6 +19,13 @@ const Navbar = () => {
                             <Nav.Link href="#home" className="navbar-item">About me</Nav.Link>
                             <Nav.Link href="#features" className="navbar-item" disabled>Portfolio</Nav.Link>
                             <Nav.Link href="#features" className="navbar-item" disabled>Contact</Nav.Link>
+                            <Nav.Link target="_blank" href="https://www.linkedin.com/in/jo%C3%A3o-victor-de-s%C3%A1-ferraz-coutinho-886944b5/" className="navbar-item"><IconContext.Provider value={{
+                                color: "#007eb3", size: "20px"
+                            }}>
+                                <div>
+                                    <FaLinkedin />
+                                </div>
+                            </IconContext.Provider></Nav.Link>
                         </Nav>
                     </Container>
                 </BNavbar.Collapse>
