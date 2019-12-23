9358532f07099d2a08b8475bec6af34dd0bd06f9
jvcoutinho <joao.coutinho9@gmail.com>
2019-12-22 21:37:22 -0300

    Setting up initial mid content and extracting font import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Accordion from './components/Accordion';
 import Navbar from './components/Navbar'
+import MidContent from './components/MidContent'
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
+      <MidContent />
     </div>
   );
 }
---
+++ src/components/MidContent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MidContent.scss'
+import Container from 'react-bootstrap/Container'
+
+const MidContent = () => {
+    return (
+        <Container id="mid-content">
+            <h1 className="display-4">Hi there, I'm João Victor</h1>
+            <h2>didi</h2>
+        </Container>
+    )
+}
+
+export default MidContent
