af68d94fed07feddfae18d2d6caba1d45ee13a6b
jvcoutinho <joao.coutinho9@gmail.com>
2019-12-24 09:21:21 -0300

    Fixing word

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -49,14 +49,14 @@ const text = () => {
                     <p>
                         The phrase above is one of the frequent-non-sense-out-of-context catch phrases people put on their social media, and it's the one I put on mine. I saw it for the first time
                         in <a href="https://store.playstation.com/pt-br/product/UP9000-NPUA80719_00-RAIN000000000001" target="_blank">Rain</a>, an awesome <i>art game</i> released in 2013.
-                        The context of this phrase was reaching a circus through some multiple paths that you had to choose which to follow, but I try to think this phrase as <q>follow your objectives, you'll get there</q> or <q>life is too short, why bothering with this</q>, and that's how I try to see life.
+                        The context of this phrase was reaching a circus through some multiple paths that you had to choose which to follow (but then, <q><i>all of the paths lead to the circus</i></q>), but I try to think this phrase as <q>follow your objectives, you'll get there</q> or <q>life is too short, why bothering with this</q>, and that's how I try to see life.
                     </p>
                     <p>
                         But who the hell is <q>I</q>? My full name is <b>João Victor de Sá Ferraz Coutinho</b>, born on 01/18/1998 at a cold city from the interior of Pernambuco, Brazil.
                         I define myself as a very <b>chill</b>, <b>focused</b> and <b>charismatic</b> person. The things I love to do include:
                         <ul>
-                            <li><b>Playing games</b> I have a special affection for <i>art games</i>, like <a>Journey</a>, <a>Gris</a> and <a>Abzû</a>, but I also enjoy some casual <a>League of Legends</a> (best
-                            Ashe/Sivir/mid Morgana from my neighbourhood) and <a>Teamfight Tactics</a>, and some dramatic AAAs (hi, <a>The Last Of Us</a> and <a>Beyond: Two Souls</a>).
+                            <li><b>Playing games</b> I have a special affection for <i>art games</i>, like <a href="https://thatgamecompany.com/journey/" target="_blank">Journey</a>, <a href="https://nomada.studio/" target="_blank">Gris</a> and <a href="https://abzugame.com/" target="_blank">Abzû</a>, but I also enjoy some casual <a href="https://br.leagueoflegends.com/pt/" target="_blank">League of Legends</a> (best
+                            Ashe/Sivir/mid Morgana from my neighbourhood) and <a href="https://br.leagueoflegends.com/pt/featured/events/teamfight-tactics" target="_blank">Teamfight Tactics</a>, and some dramatic AAAs (<a href="https://www.thelastofus.playstation.com/" target="_blank">The Last Of Us</a>, <a href="https://www.quanticdream.com/en/beyond-two-souls" target="_blank">Beyond: Two Souls</a>...).
                             </li>
                             <li><b>Watching series, cartoons and animes</b> <i>El Psy Congroo.</i>/<i>For the Honor of Grayskull!</i></li>
                             <li><b>Listening to music</b> From indie to pop, from lo-fi to funk, I like them all (except for hard metal, really sorry).</li>
@@ -65,7 +65,7 @@ const text = () => {
                         </ul>
                     </p>
                     <p>
-                        The last item is specially important because I don't code only for fun, but professionally as well. Currently I'm undergraduating <b>Computer Science</b> at <b>University Federal of Pernambuco (UFPE)</b>,
+                        The last item is specially important because I don't code only for fun, but professionally as well. Currently I'm undergraduating <b>Computer Science</b> at <b>Federal University of Pernambuco (UFPE)</b>,
                         with good knowledge and contributions of mine in <b>game development</b>, <b>software enginnering</b>, <b>computer graphics</b>, <b>artificial intelligence</b> and <b>mathematical logic and computation</b>,
                         areas that I have fascination. I plan to finish the course in late 2020.
                     </p>
